e0386283834f7982da7296adaca405f9feabd879
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-03-24 10:01:44 +0000

    Added pagination

--- src/main/java/q/jv/hospitalmanagement/web/PatientController.java
+++ src/main/java/q/jv/hospitalmanagement/web/PatientController.java
@@ -1,9 +1,12 @@
 package q.jv.hospitalmanagement.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import q.jv.hospitalmanagement.entities.Patient;
 import q.jv.hospitalmanagement.repository.PatientRepository;
 
@@ -15,9 +18,11 @@ public class PatientController {
 
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(Model model, @RequestParam(name="page",defaultValue = "0") int page,@RequestParam(name="size",defaultValue = "4") int size) {
+        Page<Patient> patientsPage = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("patients", patientsPage.getContent());
+        model.addAttribute("pages",new int[patientsPage.getTotalPages()]);
+        model.addAttribute("currentPage",page);
         return "patients";
     }
 }
