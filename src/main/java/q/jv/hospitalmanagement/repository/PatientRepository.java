ea9da418eba932dda47d5277a95136bf3f582784
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-03-24 10:18:50 +0000

    Added search

--- src/main/java/q/jv/hospitalmanagement/repository/PatientRepository.java
+++ src/main/java/q/jv/hospitalmanagement/repository/PatientRepository.java
@@ -1,7 +1,10 @@
 package q.jv.hospitalmanagement.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import q.jv.hospitalmanagement.entities.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNameContains(String keyword, Pageable pageable);
 }
--- src/main/java/q/jv/hospitalmanagement/web/PatientController.java
+++ src/main/java/q/jv/hospitalmanagement/web/PatientController.java
@@ -18,11 +18,15 @@ public class PatientController {
 
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model, @RequestParam(name="page",defaultValue = "0") int page,@RequestParam(name="size",defaultValue = "4") int size) {
-        Page<Patient> patientsPage = patientRepository.findAll(PageRequest.of(page,size));
+    public String index(Model model,
+                        @RequestParam(name="page",defaultValue = "0") int page,
+                        @RequestParam(name="size",defaultValue = "4") int size,
+                        @RequestParam(name="keyword",defaultValue = "") String keyword) {
+        Page<Patient> patientsPage = patientRepository.findByNameContains(keyword,PageRequest.of(page,size));
         model.addAttribute("patients", patientsPage.getContent());
         model.addAttribute("pages",new int[patientsPage.getTotalPages()]);
         model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
         return "patients";
     }
 }
